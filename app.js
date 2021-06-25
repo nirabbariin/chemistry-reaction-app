f4a328dcd9ba2ead6740fdbab24609ce671606e5
nirab bariin <nirabah19885307@gmail.com>
2021-06-25 11:51:22 +0600

    ready for online deployment

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 // const _ = require("lodash");
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/Chemistry",{useNewUrlParser:true});
+mongoose.connect("mongodb+srv://nirab_bariin:I'dluv2havsax@chemistry.sth7p.mongodb.net/Chemistry",{useNewUrlParser:true});
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-app.listen(3000, function() {
+app.listen(process.env.PORT||3000, function() {
   console.log("Server started on port 3000");
 });
 
